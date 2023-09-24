c74cda56c5f370537816c0ca55db7ac325c9e5e5
amennajam009 <amennajam7@gmail.com>
2023-09-24 12:25:43 +0500

    add

---
+++ Data-Type.js
@@ -0,0 +1,24 @@
+
+//secondinteger seconddecimal and secondstring  my parameters 
+
+function datatypefunction(secondInteger , secondDecimal , secondString){
+    const firstInteger = 5 
+   
+    const firstDecimal = 3.9
+
+    const firstString = 'Hello'
+
+    secondInteger = 6
+    const myFirstandSecondInteger = firstInteger + secondInteger
+    console.log(myFirstandSecondInteger)
+
+    secondDecimal = 9.5
+    const myFisrtadnSecondDecimal = firstDecimal * secondDecimal
+    console.log(myFisrtadnSecondDecimal)
+
+    secondString = 'World'
+    const myFirstandSecondString = firstString + secondString
+    console.log(myFirstandSecondString)
+}
+
+console.log(datatypefunction())
